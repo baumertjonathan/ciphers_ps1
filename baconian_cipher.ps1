bafbb17e79fc06626c2e5845246d1e4d6a4b02de
Jonathan Baumert <baumertjonathan@yahoo.com>
2021-07-01 16:07:27 -0600

    added atbash cipher

--- baconian_cipher.ps1
+++ baconian_cipher.ps1
@@ -170,10 +170,17 @@ function BaconianCipher24_Encrypt([string]$Text) {
 function BaconianCipher26_Decrypt([string]$Text) {
         <#
         .SYNOPSIS
-            Encrypts a string of text with a 26 character baconian cipher
+            Decrypts a string of text with a 26 character baconian cipher
         .DESCRIPTION
+            Decrypts a string of text consisting of sets of 5 a's and b's using a baconian cipher. 
+            These sets of strings are not deliminated and decryption works properly wth spaces. 
+            This uses an altered version of the traditional baconian cipher to account for a 26 character
+            alphabet. 
         .EXAMPLE
+            BaconianCipher26_Decrypt "aabbbaabaaababbababbabbba baabbaabbbaabaabaaabaabaa"
+            > "HELLO THERE"
         .INPUTS
+            [string] Text : The text to be decrypted using the baconian cipher.
     #>
     $Text = $Text.ToUpper();
     [string]$result = "";
@@ -197,6 +204,20 @@ function BaconianCipher26_Decrypt([string]$Text) {
 }
 
 function BaconianCipher24_Decrypt([string]$Text) {
+    <#
+        .SYNOPSIS
+            Decrypts a string of text with a 26 character baconian cipher
+        .DESCRIPTION
+            Decrypts a string of text consisting of sets of 5 a's and b's using a baconian cipher. 
+            These sets of strings are not deliminated and decryption works properly wth spaces. 
+            This traditional version of the cipher has two sets of letters that share the same 
+            code I/J and U/V.
+        .EXAMPLE
+            BaconianCipher24_Decrypt "aabbbaabaaababbababbabbba baabbaabbbaabaabaaabaabaa"
+            > "HELLO THERE"
+        .INPUTS
+            [string] Text : The text to be decrypted using the baconian cipher.
+    #>
     $Text = $Text.ToUpper();
     [string]$result = "";
     [string]$temp = "";
