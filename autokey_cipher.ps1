dcc15ad84594391bda28ea967f5830bdeae56c46
Jonathan Baumert <baumertjonathan@yahoo.com>
2021-07-01 16:47:52 -0600

    added autokey cipher

---
+++ autokey_cipher.ps1
@@ -0,0 +1,96 @@
+$tabula_recta = @(
+    ( 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ),
+    ( 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a' ),
+    ( 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b' ),
+    ( 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c' ),
+    ( 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd' ),
+    ( 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e' ),
+    ( 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f' ),
+    ( 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g' ),
+    ( 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ),
+    ( 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ),
+    ( 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ),
+    ( 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ),
+    ( 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' ),
+    ( 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ),
+    ( 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ),
+    ( 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o' ),
+    ( 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p' ),
+    ( 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q' ),
+    ( 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r' ),
+    ( 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' ),
+    ( 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't' ),
+    ( 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' ),
+    ( 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' ),
+    ( 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w' ),
+    ( 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x' ),
+    ( 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' )
+);
+
+$alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function AutokeyCipher_Encrypt([string]$text, [string]$key) {
+    <#
+        .SYNOPSIS
+            Encrypts a string using a key and the autokey cipher 
+        .DESCRIPTION
+            Encrypts a string by taking the string and a key and returning the result
+            of those two inputs on a tabula recta. The key is appended with the input allowing for
+            a shorter key than the message. 
+        .EXAMPLE
+            AutokeyCipher_Encrypt "Hello There" "Turing"
+            > ayctbzoicp
+        .INPUTS
+            [string] Text : the text to be encrypted using the autokey cipher
+            [string] Key  : the key to be used in encrypting the text. 
+    #>
+    $text = $text.ToLower();
+    $text = $text.Replace(" ", "");
+    $key = $key.ToLower();
+    $key = $key.Replace(" ", "");
+    $result = "";
+    $key += $text;
+    $p_int = 0;
+    $k_int = 0;
+    for($i = 0; $i -lt $text.Length; $i++){
+        $p_int = $alphabet.IndexOf($text[$i]);
+        $k_int = $alphabet.IndexOf($key[$i]);
+        $result += $tabula_recta[$p_int][$k_int];
+    }
+    return $result;
+}
+
+function AutokeyCipher_Decrypt([string]$Text, [string]$Key) {
+    <#
+        .SYNOPSIS
+            Decrypts a string using a key and the autokey cipher 
+        .DESCRIPTION
+            Decrypts a string by taking the string and a key and returning the result
+            of those two inputs on a tabula recta. The key is appended with the decrypted 
+            result allowing for a shorter key than message. 
+        .EXAMPLE
+            AutokeyCipher_Decrypt "ayctbzoicp" "Turing"
+            > hellothere
+        .INPUTS
+            [string] Text : the text to be decrypted using the autokey cipher
+            [string] Key  : the key to be used in decrypting the text. 
+    #>
+    $text = $text.ToLower();
+    $text = $text.Replace(" ", "");
+    $key = $key.ToLower();
+    $key = $key.Replace(" ", "");
+    $result = "";
+    $p_int = 0;
+    $k_int = 0;
+    for($i = 0; $i -lt $text.Length; $i++){
+        $k_int = $alphabet.IndexOf($key[$i]);
+        for ($j = 0; $j -lt 26; $j++){
+            if ($tabula_recta[$j][$k_int] -eq $text[$i]){
+                $p_int = $j
+            }
+        }
+        $result += $alphabet[$p_int];
+        $key+= $alphabet[$p_int];
+    }
+    return $result;
+}
