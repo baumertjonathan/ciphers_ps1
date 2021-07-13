d9d7b912e7a4d8a0db347f40418e73ab0d29351b
Jonathan Baumert <baumertjonathan@yahoo.com>
2021-07-13 15:01:07 -0600

    updated atbash cipher to single function version

--- atbash_cipher.ps1
+++ atbash_cipher.ps1
@@ -1,46 +1,48 @@
-[string]$alphabet = "abcdefghijklmnopqrstuvwxyz"
-[string]$tebahpla = "zyxwvutsrqponmlkjihgfedcba" #variable name funny, alphabet backwards
-
-function Validate([string]$Text) {
+function AtbashCipher {
     <#
         .SYNOPSIS
-            validates string for the atbash cipher
-        .DESCRIPTION
-            checks that the string contains only letters and spaces
-        .EXAMPLE
-            Validate "Hello There"
-            > True
-        .INPUTS
-            [string] Text : the string to be validated
-    #>
-    for($i = 0; $i -lt $Text.Length; $i++){
-        if ($Text[$i] -notmatch '^[a-zA-Z\s]'){
-            return $false
-        }
-    }
-    return $true
-}
-function AtbashCipher_Encrypt([string]$Text) {
-        <#
-        .SYNOPSIS
-            Encrypts a string using an Atbash Cipher
+            Encrypts or Decrypts a string using an Atbash Cipher
         .DESCRIPTION
             Encrypts a string by substituting each letter with the letter that would be in 
             that position of the alphabet if it was flipped. 
             for example "a" would be substituted by "z" and "b" by "y"
-            This, like much of powershell is case insensitive. 
+            Decryption occours by following the same process.
         .EXAMPLE
-            AtbashCipher_Encrypt "Hello There"
+            AtbashCipher "Hello There"
             > svool gsviv
+        .EXAMPLE
+            AtbashCipher "svool gsviv"
+            > hello there
         .INPUTS
-            [string] Text the string to be encrypted using the Atbash Cipher
+            [string] Text : The string to be encrypted using the Atbash Cipher
     #>
+
+    Param (
+        [string] $Text
+    )
+    
+    #Variables
+    $output = "";
+
+    #Constants
+    Set-Variable -name alphabet -value([string]"abcdefghijklmnopqrstuvwxyz") -Option Constant;
+    Set-Variable -name tebahpla -value([string]"zyxwvutsrqponmlkjihgfedcba") -Option Constant;
+    
+    #Normalize Text
     $Text = $Text.ToLower();
-    if(Validate($Text)){
-        [string]$output = "";
+    
+    #Validate
+    for($i = 0; $i -lt $Text.Length; $i++){
+        if ($Text[$i] -notmatch '^[a-zA-Z\s]'){
+            throw('Input must be letters only');
+            return;
+        }
+    }
+    #Encrypt/Decrypt
+    if(-not ($Decrypt.IsPresent)){
         for($i = 0; $i -lt $Text.Length; $i++){
             if ($Text[$i] -eq " "){
-                $output += " "
+                $output += " ";
                 continue;
             }
             $index = $alphabet.IndexOf($Text[$i]);
@@ -48,40 +50,4 @@ function AtbashCipher_Encrypt([string]$Text) {
         }
         return $output;
     }
-    else {
-        throw "Input must be letters only";
-    }
 }
-
-function AtbashCipher_Decrypt([string]$Text) {
-        <#
-        .SYNOPSIS
-            Decrypts a string using an Atbash Cipher
-        .DESCRIPTION
-            Decrypts a string by substituting each letter with the letter that would be in 
-            that position if the alphabet was flipped.
-            for example "a" would be substituted by "z" and "b" by "y"
-        .EXAMPLE
-            AtbashCipher_Decrypt "svool gsviv"
-            > hello there
-        .INPUTS
-            [string] Text : the string to be decrypted. 
-    #>
-    $Text = $Text.ToLower()
-    if(Validate($Text)){
-        [string]$output = "";
-        for($i = 0; $i -lt $Text.Length; $i++){
-            if ($Text[$i] -eq " "){
-                $output += " "
-                continue;
-            }
-            $index = $tebahpla.IndexOf($Text[$i]);
-            $output += $alphabet[$index];
-        }
-        return $output;
-    }
-    else{
-        throw "Input must be letters only";
-    }
-}
-
