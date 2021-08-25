8c166a10984d5842e1b2ae200417f35941342864
baumertjonathan <youturn98@outlook.com>
2021-08-25 09:47:17 -0600

    added trifid cipher and updated readme

--- README.md
+++ README.md
@@ -9,11 +9,40 @@ The autokey cipher is a cipher where each letter in plaintext is mapped to the [
 ## Baconian Cipher
 The Baconian cipher is a type of mesage encoding wherea as each letter of the plaintext is replaced by a group of five letters consisting of either 'a' or 'b'. [Wikipedia](https://en.wikipedia.org/wiki/Bacon%27s_cipher)
 
+## Bifid Cipher
+The Bifid cipher combines the Polybius square with transposition, it takes a key (the polybius square) and a period, used in transposition. [Wikipedia](https://en.wikipedia.org/wiki/Bifid_cipher)
+
+Here is an example of how this might work in practice with a key of "qwertyuiopasdfghklzxcvbnm", a period of 3, and text of "Testing"
+
+A Polybius square would be formed as such from the key
+q w e r t
+y u i o p
+a s d f g
+h k l z x
+c v b n m
+
+The text to be encrypted will then be put through getting the following results
+Text  : t e s t i n g
+Row   : 0 0 2 0 1 4 2
+Column: 4 2 1 4 2 3 4
+
+Then the data will be split by period
+Text  : tes tin g
+Row   : 002 014 2
+Column: 421 423 4
+
 ## Caesar Cipher
 The Caesar cipher is a type of substitution cipher where each letter is replaced by another letter a fixed number of positions away in the alphabet. [Wikipedia](https://en.wikipedia.org/wiki/Caesar_cipher)
 
-## Substitution Cipher
-A simple substitution cipher is a cipher where each letter of an alphabet is replaced by a different letter of the alphabet provided by a key. [Wikipedia](https://en.wikipedia.org/wiki/Substitution_cipher)
+## Playfair Cipher
+The Playfair cipher is a type of diagram substitution cipher where the key is maped to a 5x5 square filling in the rest of the alphabet afterward (typically i/j are combined) then the text is split into pairs and a series of rules determine how those letters are substituted using the diagram. 
+[Wikipedia](https://en.wikipedia.org/wiki/Playfair_cipher)
+
+## Polybius Cipher
+The Polybius cipher is a substitution cipher using the [Polybius Square](https://en.wikipedia.org/wiki/Polybius_square) each letter is replaced with its coordinates in the square. 
+
+## Porta Cipher
+The porta cipher is a cipher where each letter of plaintext is mapped to a tabula using a keyword, this is a tabula specific to this cipher. The key is repeated if it is shorter than the text being encoded. 
 
 ## Rail-Fence Cipher
 The rail-fence cipher is a type of transposition cipher. Each letter is taken and written in a zig zag fashion based on a numerical key, for example a message of 'hello there' with a key of 3 could be transposed as so:
@@ -25,12 +54,38 @@ h . . . o . . . r .
 which is then read from left to right providing the following ciphertext: 'horelteelh'
 [Wikipedia](https://en.wikipedia.org/wiki/Rail_fence_cipher)
 
-## Playfair Cipher
-The Playfair cipher is a type of diagram substitution cipher where the key is maped to a 5x5 square filling in the rest of the alphabet afterward (typically i/j are combined) then the text is split into pairs and a series of rules determine how those letters are substituted using the diagram. 
-[Wikipedia](https://en.wikipedia.org/wiki/Playfair_cipher)
+## Substitution Cipher
+A simple substitution cipher is a cipher where each letter of an alphabet is replaced by a different letter of the alphabet provided by a key. [Wikipedia](https://en.wikipedia.org/wiki/Substitution_cipher)
 
-## Polybius Cipher
-The Polybius cipher is a substitution cipher using the [Polybius Square](https://en.wikipedia.org/wiki/Polybius_square) each letter is replaced with its coordinates in the square. 
+## Trifid Cipher
+combines substitution and transposition ciphers, it takes a key (three squares of characters) and a period (to be used in transposition)
+[Wikipedia](https://en.wikipedia.org/wiki/Trifid_cipher)
 
-## Porta Cipher
-The porta cipher is a cipher where each letter of plaintext is mapped to a tabula using a keyword, this is a tabula specific to this cipher. The key is repeated if it is shorter than the text being encoded. 
+Here is an example with a key of "qwertyuiopasdfghjklzxcvbnm.", a period of 3, and text of "Testing"
+
+==Squares==
+  0       1       2
+q w e   p a s   l z x
+r t y   d f g   c v b 
+u i o   h j k   n m .
+
+Text  : t e s t i n g
+Box   : 0 0 1 0 0 2 1
+Row   : 1 0 0 1 2 2 1
+Column: 1 2 2 1 1 0 2
+
+Split by periods
+tes tin g
+001 002 1
+100 122 1
+122 110 2
+
+Combine
+001100122 002122110 112
+
+Encipher again
+Text  : w p k e k d g
+Box   : 0 1 1 0 1 1 1
+Row   : 0 0 2 0 2 1 1
+Column: 1 0 2 2 2 0 2
+
--- trifid_cipher.ps1
+++ trifid_cipher.ps1
@@ -1,16 +1,52 @@
-
-<#
-setup
-
-. ./trifid_cipher.ps1
-
-
-
-TrifidCipher -Text "Hello There" -Key "EPSDUCVWYM.ZLKXNBTFGORIJHAQ"
-
-#> 
-
 function TrifidCipher {
+    <#
+        .SYNOPSIS
+            Encrypts or decrypts a string of text using the trifid cipher
+        .DESCRIPTION
+            Encrypts or decrypts by first mapping 26 characters to three squares (a period being added to the alphabet), splitting the results by a period then combining the results and enciphering again
+            Here is an example with a key of "qwertyuiopasdfghjklzxcvbnm.", a period of 3, and text of "Testing"
+
+            ==Squares==
+              0       1       2
+            q w e   p a s   l z x
+            r t y   d f g   c v b 
+            u i o   h j k   n m .
+
+            Text  : t e s t i n g
+            Box   : 0 0 1 0 0 2 1
+            Row   : 1 0 0 1 2 2 1
+            Column: 1 2 2 1 1 0 2
+
+            Split by periods
+            tes tin g
+            001 002 1
+            100 122 1
+            122 110 2
+
+            Combine
+            001100122 002122110 112
+
+            Encipher again
+            Text  : w p k e k d g
+            Box   : 0 1 1 0 1 1 1
+            Row   : 0 0 2 0 2 1 1
+            Column: 1 0 2 2 2 0 2
+
+            The key must have 26 letters and a period.
+            The period cannot be longer than the text being enciphered. 
+        .EXAMPLE
+            TrifidCipher -Text "Hello There" -Key "zxcvbnm.asdfghjklqwertyuiop" -Period 5
+            > llayhuosxl
+        .EXAMPLE
+            TrifidCipher -Text "llayhuosxl" -Key "zxcvbnm.asdfghjklqwertyuiop" -Period 5 -Decrypt
+            > hellothere
+        .INPUTS
+            [switch] Decrypt : if present decrypts rather than encrypts the string of text
+            [string] Text    : The string of text to be encrypted or decrypted
+            [string] Key     : The key to be used in encrypting or decrypting the text
+            [int]    Period  : The period to be used in encrypting or decrypting the text, must be less than the length of the text to be enciphered.
+    #>
+
     Param(
         [switch]$Decrypt,
         [string]$Text,
@@ -54,63 +90,86 @@ function TrifidCipher {
             }
         }
     }
-    Write-Host($boxes);
     #Encode
-    for($l = 0; $l -lt $Text.Length; $l++){
-        for($b = 0; $b -lt 3; $b++){
-            for($i = 0; $i -lt 3; $i++){
-                for($j = 0; $j -lt 3; $j++){
-                    if($boxes[$b][$i, $j] -eq $Text[$l]){
-                        $EncodedArray += ,@($b, $i, $j); #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.1#comma-operator-
+    if(-not $Decrypt.IsPresent){
+        for($l = 0; $l -lt $Text.Length; $l++){
+            for($b = 0; $b -lt 3; $b++){
+                for($i = 0; $i -lt 3; $i++){
+                    for($j = 0; $j -lt 3; $j++){
+                        if($boxes[$b][$i, $j] -eq $Text[$l]){
+                            $EncodedArray += ,@($b, $i, $j); #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.1#comma-operator-
+                        }
                     }
                 }
             }
         }
-    }
-
-    #split into periods
-    $splitCounter = 0;
-    $tempPeriod = @();
-    for($i = 0; $i -lt $EncodedArray.Length; $i++){
-        while($splitCounter -lt $Period -and ($i -lt $EncodedArray.Length)){
-            $tempPeriod += ,$EncodedArray[$i];
-            $splitCounter++;
-            $i++;
-        }
-        $splitCounter = 0;
-        $PeriodArray += ,$tempPeriod;
-        $tempPeriod = @();
-        $i--;
-    }
-
-    foreach($PeriodSection in $PeriodArray){
         for($i = 0; $i -lt 3; $i++){
-            for($j = 0; $j -lt $PeriodSection.Length; $j++){
-                Write-Host($PeriodSection[$j][$i]);
-                $PeriodString += $PeriodSection[$j][$i];
+            $temp = "";
+            for($j = 0; $j -lt $EncodedArray.Length; $j++){
+                $temp += $EncodedArray[$j][$i];
+            }
+            $temp = "";
+        }
+        #split by periods
+        $splitCounter = 0;
+        $tempPeriod = @();
+        $rowPeriod = @();
+        $columnPeriod = @();
+        for($i = 0; $i -lt $Text.Length; $i++){
+            while($splitCounter -lt $Period -and ($i -lt $Text.Length)){
+                $tempPeriod += $EncodedArray[$i][0]; #box
+                $rowPeriod += $EncodedArray[$i][1]; #row
+                $columnPeriod += $EncodedArray[$i][2]; #column
+                $splitCounter++;
+                $i++;
+            }
+            $splitCounter = 0;
+            $tempPeriod += $rowPeriod;
+            $tempPeriod += $columnPeriod;
+            $PeriodArray += ,$tempPeriod;
+            $tempPeriod = @();
+            $rowPeriod = @();
+            $columnPeriod = @();
+            $i--;
+        }
+        foreach ($period1 in $PeriodArray) {
+            for($i = 0; $i -lt $period1.Length; $i+=3){
+                $output += $boxes[$period1[$i]][$period1[$i+1], $period1[$i+2]];
             }
         }
-        $PeriodString += " ";
     }
-    Write-Host("period string:", $PeriodString);
-
-    $counter = 0;
-    for($i = 0; $i -lt $PeriodString.Length; $i ++){
-        while($counter -lt 3){
-            $counter++;
-        }
-        $counter = 0;
-    }
-
-    Write-Host($Text);
-    for($i = 0; $i -lt 3; $i++){
-        $temp = "";
-        for($j = 0; $j -lt $EncodedArray.Length; $j++){
-            $temp += $EncodedArray[$j][$i];
-        }
-        Write-Host($temp);
-        $temp = "";
-    }
-    
     #Decrypt
+    else{
+        $splitPeriodArray = @();
+        $periodCounter = 0;
+        for($i = 0; $i -lt $Text.Length; $i++){
+            [string]$tempPeriod = ""
+            $periodCounter = 0;
+            while($periodCounter -lt $Period){
+                :box for($b = 0; $b -lt 3; $b++){
+                    :row for($r = 0; $r -lt 3; $r++){
+                        :column for($c = 0; $c -lt 3; $c++){
+                            if($Text[$i] -eq $boxes[$b][$r, $c]){
+                                $tempPeriod += $b;
+                                $tempPeriod += $r;
+                                $tempPeriod += $c
+
+                                break box;
+                            }
+                        }
+                    }
+                }
+                $i++
+                $periodCounter ++
+            }
+            $i--;
+            $splitPeriodArray += $tempPeriod;
+        }
+        foreach ($periodSection in $splitPeriodArray) {
+            for($i = 0; $i -lt $periodSection.Length/3; $i++){
+                $output += $boxes[[convert]::ToInt32($periodSection[$i], 10)][[convert]::ToInt32($periodSection[$i+$periodSection.Length/3], 10), [convert]::ToInt32($periodSection[$i+(($periodSection.Length/3)*2)], 10)];
+            }
+        }
+    }
+    return $output;
 }
